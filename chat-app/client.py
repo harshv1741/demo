aae69479bdbef5d1cac11ee8d7c541be3847bc73
harshv1741 <harshv1741@gmail.com>
2023-02-15 12:01:53 +0530

    Initial Commit

--- chat-app/client.py
+++ chat-app/client.py
@@ -2,37 +2,36 @@
 
 # Importing Libraries
 import socket
-import select
 import sys
-
-from thread import *
+import select
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(server.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 if len(sys.argv) != 3:
-    print ("Correct usage: script, IP address, port number")
+    print("Correct usage: script, IP address, port number")
     exit()
 
 IP_address = str(sys.argv[1])
-Port = int(sysv.argv[2])
+Port = int(sys.argv[2])
 
+server.connect((IP_address,Port))
 
-server.bind((IP_address, Port))
+while True:
 
+    sockets_list = [sys.stdin, server]
 
-server.listen(100)
+    read_sockets, write_socket, error_socket =  select.select(sockets_list,[],[])
 
-list_of_clients = []
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048)
+            print(message)
 
+        else:
+            message = socks.recv(2048)
+            server.send(message)
+            sys.stdout.write("{you}")
+            sys.stdout.write(message)
+            sys.stdout.flush()
 
-def clientthread(conn, addr):
-
-    conn.send("---- Welcome to chatroom \"Godzilla\"! ----")
-
-    while True:
-
-        if msg:
-            print("{"+addr[0]+"}"+ msg)
-
-            msg_to_send = "{"+addr[0]+"}" 
+server.close()
